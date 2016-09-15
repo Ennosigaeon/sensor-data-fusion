26a0a68c183de1c4eee4196fdc826d0e0ea7130d
Marc <marczoeller@gmx.net>
2016-09-15 17:06:54 +0200

    Add scripts for calibration and marker detection

---
+++ calibrateCamera.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+./bin/aruco_calibrate_camera -d=0 -h=5 -l=0.033 -s=0.033 -w=8 ../Contrib/calibration.yml
+
---
+++ detectMarker.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./bin/aruco_detect_markers -d=0 -l=0.11 -c=../Contrib/calibration.yml

c1c3c01a1798dc8a26daca44b04bd60a348f9243
Marc <marczoeller@gmx.net>
2016-09-16 17:44:02 +0200

    Adapt script to new location

--- bin/calibrateCamera.sh
+++ bin/calibrateCamera.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-./bin/aruco_calibrate_camera -d=0 -h=5 -l=0.033 -s=0.033 -w=8 ../Contrib/calibration_webcam.yml
+./aruco_calibrate_camera -d=0 -h=5 -l=0.033 -s=0.033 -w=8 ../../Contrib/calibration_webcam.yml
 

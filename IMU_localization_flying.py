4ca6a8af67cf3fc99b0fb7038aeefc76693eb90f
Marc <marczoeller@gmx.net>
2016-09-19 16:33:14 +0200

    Use function of extDrone

--- IMU_localization_flying.py
+++ IMU_localization_flying.py
@@ -2,11 +2,15 @@
 
 # TODO: test this code
 
-import time, sys, math
-import extDrone
 import datetime
-import matplotlib.pyplot as plt
+import math
 import os
+import sys
+import time
+
+import matplotlib.pyplot as plt
+
+import extDrone
 
 
 def createFolder(path):
@@ -22,23 +26,23 @@ class DeadReckoning:
         self.x = 0
         self.y = 0
         self.z = 0
-        self.lastX = 0
-        self.lastY = 0
+        self.historyX = [0]
+        self.historyY = [0]
         self.timeStart = datetime.datetime.now()
         self.lastTimestamp = datetime.datetime.now()
         self.phio = drone.NavData["demo"][2][2]
 
     def updatePos(self, navData):
         # adjusting expected speed
-        vx = drone.NavData["demo"][4][0]
-        vy = drone.NavData["demo"][4][1]
-        vz = drone.NavData["demo"][4][2]
+        vx = navData["demo"][4][0]
+        vy = navData["demo"][4][1]
+        vz = navData["demo"][4][2]
 
         # adjusting angle (phid is in degree, phi is in radian)
-        phid = drone.NavData["demo"][2][2]
+        phid = navData["demo"][2][2]
         phi = ((phid - self.phio) / 180) * math.pi
 
-        z = drone.NavData["demo"][3]
+        z = navData["demo"][3]
 
         # measuring total time and time since last datapoint
         time = datetime.datetime.now()
@@ -50,126 +54,54 @@ class DeadReckoning:
         self.x += (math.cos(phi) * vx - math.sin(phi) * vy) * diff / 1000000
         self.y -= (math.cos(phi) * vy + math.sin(phi) * vx) * diff / 1000000
 
-        # plot new datapoint
-        # TODO better plotting algorithm
-        plt.plot([self.lastX, self.x], [self.lastY, self.y])
-        plt.pause(0.05)
-        self.lastX = self.x
-        self.lastY = self.y
+        self.historyX.append(self.x)
+        self.historyY.append(self.y)
 
-    def initOutputFiles(self):
-        createFolder("./data")
+    # TODO implement
+    def storeRaw(self, file):
+        pass
 
-        # plot_file is an executable file plotting the calculated x and y values
-        # raw_file stores all gathered data from the drone as well as the corresponding timestamps
-        minfree = 1;
-        while os.path.lexists("./data/plotdata" + str(minfree) + ".py"): minfree += 1
-        while os.path.lexists("./data/rawdata" + str(minfree) + ".txt"): minfree += 1
-        plot_file = open("./data/plotdata" + str(minfree) + ".py", "w")
-        raw_file = open("./data/rawdata" + str(minfree) + ".txt", "w")
+    def loadRaw(self):
+        pass
 
-        print "Create output file: ./data/plotdata" + str(minfree) + ".py"
-        print "Create output file: ./data/rawdata" + str(minfree) + ".txt"
-
-        plot_file.write("#!/usr/bin/env python\n\n")
-        plot_file.write("import matplotlib.pyplot as plt\n")
-        plot_file.write("x=[];y=[];")
-        return (plot_file, raw_file)
-
+    # noinspection PyMethodMayBeStatic
     def initRTPlot(self):
         plt.axis([-4000, 4000, -4000, 4000])
         plt.ion()
+        plt.pause(0.05)
 
-
-def extendedStart():
-    # drone start routine
-    drone = extDrone.Drone()
-    drone.startup()
-    drone.reset()
-    drone.useDemoMode(True)
-    drone.getNDpackage(["demo"])
-    time.sleep(0.5)
-    drone.trim()
-    time.sleep(1)
-    speed = 0.05
-    drone.setSpeed(speed)
-    return drone
+    def updateRTPlot(self):
+        # plot new datapoint
+        # TODO better plotting algorithm
+        plt.plot([self.historyX[-1], self.x], [self.historyY[-1], self.y], facecolor="b")
+        plt.pause(0.05)
 
 
 if (__name__ == "__main__"):
-    drone = extendedStart()
+    drone = extDrone.Drone()
+    drone.startup()
     DR = DeadReckoning(drone)
-    plot_file, raw_file = DR.initOutputFiles()
     DR.initRTPlot()
-
-    landed = True
-    speed = 0.05
 
     while (1):
         navData = drone.getNextDataSet()
 
         DR.updatePos(navData)
+        DR.updateRTPlot()
 
         key = drone.getKey()
         if key:
-            if key == ' ':
-                if landed:
-                    drone.takeoff()
-                    while drone.NavData["demo"][0][2]: time.sleep(0.1)
-                    print " drone is now in air"
-                    landed = False
-                else:
-                    drone.stop()
-                    drone.land()
-                    time.sleep(3)
-                    print " drone has landed"
-                    landed = True
-            elif key == 'q':
-                drone.turnLeft()
-                print " drone turns left"
-            elif key == 'w':
-                drone.moveForward()
-                print " drone flies forwards"
-            elif key == 'e':
-                drone.turnRight()
-                print " drone turns right"
-            elif key == 'a':
-                drone.moveLeft()
-                print " drone flies to the left"
-            elif key == 's':
-                drone.moveBackward()
-                print " drone flies backwards"
-            elif key == 'd':
-                drone.moveRight()
-                print " drone flies to the right"
-            elif key == '0':
-                print " end program"
-                drone.stop()
-                drone.land()
-                time.sleep(3)
-                print "  drone has landed"
-                plot_file.write("plt.plot(x,y);\n")
-                plot_file.write("plt.show();")
-                plot_file.close()
-                raw_file.close()
+            drone.simplePiloting(key)
+            if (key == '0'):
+                print "Program stopped"
+                drone.failSafeStopDrone()
+
+                minFree = 1
+                while os.path.lexists("./data/rawdata" + str(minFree) + ".txt"): minFree += 1
+                with open("./data/rawdata" + str(minFree) + ".txt", "w") as raw_file:
+                    DR.storeRaw(raw_file)
+
                 plt.pause(5)
                 sys.exit()
-            elif key == '+':
-                if speed <= 0.99: speed += 0.01
-                drone.setSpeed(speed)
-                print " drone speed is now " + str(speed)
-            elif key == '-':
-                if speed > 0.01: speed -= 0.01
-                drone.setSpeed(speed)
-                print " drone speed is now " + str(speed)
-            elif key == 'u':
-                drone.moveUp(speed)
-                print " drone moves up"
-            elif key == 'j':
-                drone.moveDown(speed)
-                print " drone moves down"
-            else:
-                drone.stop()
-                print " drone stopped"
         else:
             time.sleep(0.01)

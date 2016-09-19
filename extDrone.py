7488ec7940c0647a0ae90a5cf0e80c2919c3a98f
Marc <marczoeller@gmx.net>
2016-09-19 16:32:42 +0200

    Add simple piloting and extended startup

--- extDrone.py
+++ extDrone.py
@@ -13,6 +13,20 @@ class Drone(ps_drone.Drone):
         self.groundCamEnabled = True
         self.lastIMC = self.VideoImageCount
         self.lastNDC = self.NavDataCount
+        self.landed = True
+
+    def startup(self):
+        """
+        Extended startup procedure.
+        """
+        super(Drone, self).startup()
+        self.reset()
+        self.useDemoMode(True)
+        self.getNDpackage(["demo"])
+        time.sleep(0.5)
+        self.trim()
+        time.sleep(1)
+        self.setSpeed(0.05)
 
     def reset(self):
         """
@@ -36,9 +50,9 @@ class Drone(ps_drone.Drone):
             time.sleep(0.01)
         self.lastIMC = self.VideoImageCount
         return self.VideoImage
-        
+
     def getNextDataSet(self):
-        while self.NavDataCount==self.lastNDC: time.sleep(0.001) #waiting for new datapoint
+        while self.NavDataCount == self.lastNDC: time.sleep(0.001)  # waiting for new datapoint
         self.lastNDC = self.NavDataCount
         return self.NavData
 
@@ -64,3 +78,58 @@ class Drone(ps_drone.Drone):
         """
         self.groundCamEnabled = not self.groundCamEnabled
         self.groundCam(self.groundCamEnabled)
+
+    def failSafeStopDrone(self):
+        self.stop()
+        self.land()
+        time.sleep(3)
+        self.landed = True
+
+    def simplePiloting(self, key):
+        if (key is None):
+            return
+
+        if key == ' ':
+            if self.landed:
+                self.takeoff()
+                while self.NavData["demo"][0][2]: time.sleep(0.1)
+                print " drone is now in air"
+                self.landed = False
+            else:
+                self.failSafeStopDrone()
+                print " drone has landed"
+        elif key == 'q':
+            self.turnLeft()
+            print " drone turns left"
+        elif key == 'w':
+            self.moveForward()
+            print " drone flies forwards"
+        elif key == 'e':
+            self.turnRight()
+            print " drone turns right"
+        elif key == 'a':
+            self.moveLeft()
+            print " drone flies to the left"
+        elif key == 's':
+            self.moveBackward()
+            print " drone flies backwards"
+        elif key == 'd':
+            self.moveRight()
+            print " drone flies to the right"
+        elif key == '+':
+            self.__speed = min(1, self.__speed + 0.01)
+            self.setSpeed(self.__speed)
+            print " drone speed is now " + str(self.__speed)
+        elif key == '-':
+            self.__speed = max(0, self.__speed - 0.01)
+            self.setSpeed(self.__speed)
+            print " drone speed is now " + str(self.__speed)
+        elif key == 'u':
+            self.moveUp()
+            print " drone moves up"
+        elif key == 'j':
+            self.moveDown()
+            print " drone moves down"
+        else:
+            self.stop()
+            print " drone stopped"
